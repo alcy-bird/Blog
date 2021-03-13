a70189bf4ed59445bcc479924be127ad99b6db09
alcy-bird <66966168+alcy-bird@users.noreply.github.com>
2021-03-13 23:13:01 +0300

    Blog

---
+++ AdminBlog/Controllers/BlogController.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdminBlog.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System;
+using AdminBlog.Filter;
+
+namespace AdminBlog.Controllers
+{
+    [UserFilter]
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+        private readonly BlogContext _context;
+        public BlogController(ILogger<BlogController> logger, BlogContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult BlogList()
+        {
+           var List = _context.Blog.ToList();
+            return View(List);
+        }
+        public IActionResult IsPublish(int Id)
+        {
+            var blog = _context.Blog.Find(Id);
+            blog.IsPublish = true;
+            _context.Update(blog);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(BlogList));
+        }
+        public IActionResult Blog()
+        {
+            ViewBag.Categories = _context.Category.Select(w =>
+              new SelectListItem
+              {
+                  Text = w.Name,
+                  Value = w.Id.ToString()
+              }).ToList();
+            return View();
+        }
+        public async Task<IActionResult> Save(Blog model)
+        {
+            if(model != null)
+            {
+                var file = Request.Form.Files.First();
+                //C:\Users\aleyn\source\repos\MyBlog\wwwroot\img
+                string savePath = Path.Combine("C:", "Users", "aleyn", "source", "repos", "MyBlog", "wwwroot", "img");
+                var fileName = $"{DateTime.Now:MMddHHmmss}.{file.FileName.Split(".").Last()}";
+                var fileUrl = Path.Combine(savePath, fileName);
+
+                using(var fileStream=new FileStream(fileUrl, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                model.ImagePath = fileName;
+                model.AuthorId = (int)HttpContext.Session.GetInt32("id");
+
+                await _context.AddAsync(model);
+                await _context.SaveChangesAsync();
+
+                return Json(true);
+            }
+            return Json(false);
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AdminBlog/Migrations/20210311162911_updateblog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminBlog.Migrations
+{
+    public partial class updateblog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blog_Category_CategoryId",
+                table: "Blog");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryIdnt",
+                table: "Blog");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Blog",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateTime",
+                table: "Blog",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blog_Category_CategoryId",
+                table: "Blog",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blog_Category_CategoryId",
+                table: "Blog");
+
+            migrationBuilder.DropColumn(
+                name: "CreateTime",
+                table: "Blog");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Blog",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryIdnt",
+                table: "Blog",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blog_Category_CategoryId",
+                table: "Blog",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
